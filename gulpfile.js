e31c6d3ba628c18e2b8dd2de0857ae6b49f22a74
dragon-PC\dragon <proweb.developer2016@yandex.com>
2017-08-09 02:59:37 +0800

    add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var $    = require('gulp-load-plugins')();
+
+var sassPaths = [
+  'bower_components/bootstrap/scss'
+];
+
+gulp.task('sass', function() {
+  return gulp.src('scss/**/*.scss')
+    .pipe($.sass({
+      includePaths: sassPaths,
+      outputStyle: 'compressed' // if css compressed **file size**
+    })
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9']
+    }))
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('default', ['sass'], function() {
+  gulp.watch(['scss/**/*.scss'], ['sass']);
+});
